b7b4914aba9c594155ba0d4ed06fc20c48a02454
TANDRAWIJAYA, ALVIN <atandraw@fmi.com>
2018-12-05 21:03:28 +0700

    Init commit

---
+++ AlvinWeb/AlvinWeb/wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿$('.nav-right-toggle-alvin').on('click', function () {
+    $('.nav-alvin').toggleClass('expand');
+});
+$('.nav-toggler-alvin').on('click', function () {
+    $('.nav-alvin .nav-pills .nav-link').toggleClass('hidden'); $('.nav-alvin .nav-pills').toggleClass('hidden'); $('.nav-right-toggle-alvin').toggleClass('hidden');
+})
